f312b9279ccc1721d59ca4a70165aba4a2e2f52a
Martin Albert <martin.albert.187@gmail.com>
2022-08-04 18:57:07 +0200

    wip: feat: add logic for determining impact

--- src/nock/analyzer.ts
+++ src/nock/analyzer.ts
@@ -1,6 +1,11 @@
 import { CompleteSuperfaceTestConfig } from '../superface-test.interfaces';
-import { IErrorCollector } from './error-collector.interfaces';
+import {
+  IErrorCollector,
+  MatchError,
+  MatchErrorKind,
+} from './error-collector.interfaces';
 
+export type MatchImpact = 'major' | 'minor' | 'patch';
 export interface AnalysisResult {
   profile: string;
   provider: string;
@@ -8,35 +13,56 @@ export interface AnalysisResult {
 
   recordingPath: string;
 
-  // TODO: better naming - major/minor/patch
-  impact: string;
+  impact: MatchImpact;
 
   errors: string[];
 }
 
 export class Analyzer {
+  private static formattedErrors: string[] = [];
+
   static run(
     sfConfig: CompleteSuperfaceTestConfig,
-    errors: IErrorCollector
+    collector: IErrorCollector
   ): AnalysisResult {
-    const formattedErrors: string[] = [];
-
-    for (const error of errors.get()) {
-      formattedErrors.push(error.message);
-    }
-
     return {
       profile: sfConfig.profile.configuration.id,
       provider: sfConfig.provider.configuration.name,
       useCase: sfConfig.useCase.name,
-      recordingPath: errors.recordingPath,
-
-      impact: 'major',
-
-      errors: formattedErrors,
+      recordingPath: collector.recordingPath,
+      impact: this.resolveImpact(collector.get()),
+      errors: this.formattedErrors,
     };
   }
 
   // implement rules for determining impact of change
   // consider different situations where request or/and response changes
+
+  private static resolveImpact(
+    errors: (MatchError & { message: string })[]
+  ): MatchImpact {
+    let impact: MatchImpact = 'patch';
+
+    for (const error of errors) {
+      this.formattedErrors.push(error.message);
+    }
+
+    if (
+      (errors.find(error => error.kind === MatchErrorKind.LENGTH) ||
+      errors.find(error => error.kind === MatchErrorKind.METHOD) || 
+      errors.find(error => error.kind === MatchErrorKind.STATUS) || 
+      errors.find(error => error.kind === MatchErrorKind.BASE_URL) || 
+      errors.find(error => error.kind === MatchErrorKind.PATH) || 
+      errors.find(error => error.kind === MatchErrorKind.RESPONSE_HEADERS) ||
+      errors.find(error => error.kind === MatchErrorKind.REQUEST_HEADERS) || 
+      errors.find(error => error.kind === MatchErrorKind.REQUEST_BODY)) &&
+      errors.find(error => error.kind === MatchErrorKind.RESPONSE)
+    ) {
+      impact = 'major';
+    } else {
+      impact = 'minor';
+    }
+
+    return impact;
+  }
 }
